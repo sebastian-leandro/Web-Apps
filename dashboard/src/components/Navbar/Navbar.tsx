cc92ecd52a2439a99289f50d3b8ae1cdd986779a
sebastian-leandro <sebas.officciall@gmail.com>
2024-02-01 12:20:01 -0300

    update navbar

--- dashboard/src/components/Navbar/Navbar.tsx
+++ dashboard/src/components/Navbar/Navbar.tsx
@@ -1,13 +1,24 @@
+import { useState, useEffect } from 'react'
+
 import { RiMenu3Line } from 'react-icons/ri'
 import { CiSearch, CiBellOn } from 'react-icons/ci'
 
 import classes from './Navbar.module.scss'
 
 function Navbar (): React.ReactNode {
+  const [theme, setTheme] = useState(false)
+  useEffect(() => {
+    const handleTheme = (): void => {
+      if (theme) document.body.classList.add('dark')
+      else document.body.classList.remove('dark')
+    }
+    handleTheme()
+  }, [theme])
+
   return (
     <>
       <nav>
-        <RiMenu3Line className={classes.bx} />
+        <RiMenu3Line className={`${classes.bx} ${theme ? classes.light : ''}`} />
         <form action="#">
           <div className={classes.inputs}>
             <input type='search' placeholder='search...' />
@@ -16,7 +27,7 @@ function Navbar (): React.ReactNode {
             </button>
           </div>
         </form>
-        <input type="checkbox" id='theme' className={classes.theme} onClick={(): void => { document.body.classList.toggle('dark') }} hidden />
+        <input type="checkbox" id='theme' className={classes.theme} onClick={(): void => { setTheme(prevTheme => !prevTheme) }} hidden />
         <label htmlFor="theme" className={classes.toggle}></label>
         <a href="#" className={classes.notify}>
           <CiBellOn className={`${classes.bx} ${classes.bell} `} />
